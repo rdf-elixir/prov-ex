52cf8f6c7c88ddc0f0054fe7fe7ef1be0004134c
Marcel Otto <marcelotto@gmx.de>
2024-08-07 13:25:13 +0200

    Add docs

--- lib/prov.ex
+++ lib/prov.ex
@@ -2,6 +2,11 @@ defmodule PROV do
   @moduledoc """
   Implementation of the W3C PROV vocabulary.
 
+  The following features are provided:
+
+  - The `PROV` module acts as the `RDF.Vocabulary.Namespace` for the PROV vocabulary.
+  - Grax structures for all PROV classes and their properties.
+
     See <http://www.w3.org/TR/prov-overview/>.
   """
 
